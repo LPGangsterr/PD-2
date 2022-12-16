58ee59688426f207d20e25bf97410ecca4bc325d
Waleed <waah805@gmail.com>
2022-12-16 12:34:05 +0400

    PD2

--- Task 4.cpp
+++ Task 4.cpp
@@ -1,12 +1,13 @@
-#include <iostream
+#include <iostream>
 using namespace std;
 main(){
 cout << " _______" << endl;
-cout << "| _____ |       #       &&&&&&&   oo       oo       ()        " << endl;
-cout << "||     ||     #   #    &       &  o  o   o  o     ()  ()        " << endl;
-cout << "||_____||   #       #  &          o   o o   o   ()      ()     " << endl;
-cout << "||______|   #       #  &          o    O    o   ()           " << endl;
-cout << "||          #       #  &          o         o   ()          " << endl;
-cout << "||          #########  &          o         o   ()           " << endl;
-cout << "||          #       #  &       &  o         o   ()          " << endl;
-cout << "||          #       #   &&&&&&&   O         O             " << endl;
+cout << "| _____ |       #       &&&&&&&   oo       oo       ()                " << endl;
+cout << "||     ||     #   #    &       &  o  o   o  o     ()  ()    %%      %      " << endl;
+cout << "||_____||   #       #  &          o   o o   o   ()      ()  %  %    %       " << endl;
+cout << "||______|   #       #  &          o    O    o   ()      ()  %   %   %      " << endl;
+cout << "||          #       #  &          o         o   ()()()()()  %    %  %     " << endl;
+cout << "||          #########  &          o         o   ()      ()  %     % %     " << endl;
+cout << "||          #       #  &       &  o         o   ()      ()  %      %%     " << endl;
+cout << "||          #       #   &&&&&&&   O         O   ()      ()  %       %    " << endl;
+}
