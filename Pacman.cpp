6920e051b51af931f87ef6c58c83b5136beafc7d
Waleed <waah805@gmail.com>
2022-12-16 12:22:41 +0400

    PD2

---
+++ Pacman.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "     ........    " << endl;
+cout << "   .'        '.  " << endl;
+cout << "   . .'.    .'  0 0 0" << endl;
+cout << "   .  '      '. o o o" << endl;
+cout << "   .         .'  " << endl;
+cout << "    '.......'    " << endl;
+}
---
+++ Task 4.cpp
@@ -0,0 +1,12 @@
+#include <iostream
+using namespace std;
+main(){
+cout << " _______" << endl;
+cout << "| _____ |       #       &&&&&&&   oo       oo       ()        " << endl;
+cout << "||     ||     #   #    &       &  o  o   o  o     ()  ()        " << endl;
+cout << "||_____||   #       #  &          o   o o   o   ()      ()     " << endl;
+cout << "||______|   #       #  &          o    O    o   ()           " << endl;
+cout << "||          #       #  &          o         o   ()          " << endl;
+cout << "||          #########  &          o         o   ()           " << endl;
+cout << "||          #       #  &       &  o         o   ()          " << endl;
+cout << "||          #       #   &&&&&&&   O         O             " << endl;
